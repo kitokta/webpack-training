4b0deddefac195ee2b459e6843e225cd8ad491a4
Gabi Zanini <psicologia.plus@gmail.com>
2022-08-05 12:04:20 -0400

    joke api

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,3 +1,13 @@
+import axios from 'axios';
+
 export default function generateJoke() {
-    return "É pavê ou pacomê?"
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+
+    axios.get('https://icanhazdadjoke.com', config).then(res => {
+        document.getElementById('joke').innerHTML = res.data.joke;
+    })
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import generateJoke from "./generateJoke";
 import './styles/main.scss'
 import laughing from './assets/laughing.svg'
 
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
 
-console.log(generateJoke());
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+generateJoke();
